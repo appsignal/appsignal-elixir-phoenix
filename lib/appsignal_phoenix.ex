f491135df2faeb79235f145068c298d7592eb925
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-03-19 16:46:07 +0100

    Remove generated code from Appsignal.Phoenix(Test)

--- lib/appsignal_phoenix.ex
+++ lib/appsignal_phoenix.ex
@@ -1,18 +1,2 @@
 defmodule Appsignal.Phoenix do
-  @moduledoc """
-  Documentation for Appsignal.Phoenix.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Appsignal.Phoenix.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/appsignal_phoenix_test.exs
+++ test/appsignal_phoenix_test.exs
@@ -1,8 +1,4 @@
 defmodule Appsignal.PhoenixTest do
   use ExUnit.Case
   doctest Appsignal.Phoenix
-
-  test "greets the world" do
-    assert Appsignal.Phoenix.hello() == :world
-  end
 end
