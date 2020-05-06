fd2c53187405c1141183faa2683dc859f673314c
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-05-06 09:45:49 +0200

    Add module documentation to Appsignal.Phoenix.Channel

--- lib/appsignal_phoenix/channel.ex
+++ lib/appsignal_phoenix/channel.ex
@@ -1,4 +1,33 @@
 defmodule Appsignal.Phoenix.Channel do
+  @moduledoc """
+  Instruments Phoenix channels.
+
+  ## Usage
+
+  To instrument a Phoenix channel, wrap the contents of your handle_in/3
+  function in an `Appsignal.Phoenix.Channel.instrument/5` call:
+
+      defmodule AppsignalPhoenixExampleWeb.RoomChannel do
+        use Phoenix.Channel
+
+        def join("room:lobby", _message, socket) do
+          {:ok, socket}
+        end
+
+        def join("room:" <> _private_room_id, _params, _socket) do
+          {:error, %{reason: "unauthorized"}}
+        end
+
+        def handle_in("new_msg", %{"body" => body} = params, socket) do
+          Appsignal.Phoenix.Channel.instrument(__MODULE__, "new_msg", params, socket, fn ->
+            broadcast!(socket, "new_msg", %{body: body})
+            {:noreply, socket}
+          end)
+        end
+      end
+
+  """
+
   @tracer Application.get_env(:appsignal, :appsignal_tracer, Appsignal.Tracer)
   @span Application.get_env(:appsignal, :appsignal_span, Appsignal.Span)
 
