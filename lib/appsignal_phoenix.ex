bab2a1b17779442b09269326baece2015a605c08
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-05-06 08:11:39 +0200

    Add module documentation to Appsignal.Phoenix

--- lib/appsignal_phoenix.ex
+++ lib/appsignal_phoenix.ex
@@ -1,4 +1,22 @@
 defmodule Appsignal.Phoenix do
+  @moduledoc """
+  AppSignal's Phoenix instrumentation instruments calls to Phoenix applications
+  to gain performance insights and error reporting.
+
+  ## Installation
+
+  To install `Appsignal.Phoenix` into your Phoenix application, `use
+  Appsignal.Phoenix` in your application's endpoint module:
+
+      defmodule AppsignalPhoenixExampleWeb.Endpoint do
+        use Phoenix.Endpoint, otp_app: :appsignal_phoenix_example
+        use Appsignal.Phoenix
+
+        # ...
+      end
+
+  """
+
   defmacro __using__(_) do
     quote do
       use Appsignal.Plug
