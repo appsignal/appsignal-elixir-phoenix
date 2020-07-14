ca51581610deca3c4cc5af0264606c9257df2026
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-07-14 12:48:24 +0200

    Add missing aliases in EExEngine, ExsEngine and Appsignal modules

--- lib/appsignal_phoenix/template/eex_engine.ex
+++ lib/appsignal_phoenix/template/eex_engine.ex
@@ -1,10 +1,13 @@
 defmodule Appsignal.Phoenix.Template.EExEngine do
+  alias Appsignal.Phoenix.Template
+  alias Phoenix.Template.EExEngine
+
   @behaviour Phoenix.Template.Engine
   @moduledoc false
 
   def compile(path, name) do
     path
-    |> Phoenix.Template.EExEngine.compile(name)
-    |> Appsignal.Phoenix.Template.compile(path)
+    |> EExEngine.compile(name)
+    |> Template.compile(path)
   end
 end
