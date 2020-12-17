03ef4e87373d6fb38d97aa31207603d2fd8bd5f0
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-12-17 09:16:01 +0100

    Duplicate Phoenix.Channel into Phoenix.LiveView

--- lib/appsignal_phoenix/live_view.ex
+++ lib/appsignal_phoenix/live_view.ex
@@ -1,6 +1,44 @@
 defmodule Appsignal.Phoenix.LiveView do
-  defdelegate instrument(module, name, socket, fun), to: Appsignal.Phoenix.Channel
-  defdelegate instrument(module, name, params, socket, fun), to: Appsignal.Phoenix.Channel
+  @tracer Application.get_env(:appsignal, :appsignal_tracer, Appsignal.Tracer)
+  @span Application.get_env(:appsignal, :appsignal_span, Appsignal.Span)
+
+  def instrument(module, name, socket, fun) do
+    instrument(module, name, %{}, socket, fun)
+  end
+
+  def instrument(module, name, params, socket, fun) do
+    Appsignal.instrument(
+      "#{Appsignal.Utils.module_name(module)}##{name}",
+      fn span ->
+        _ = @span.set_namespace(span, "channel")
+
+        try do
+          fun.()
+        catch
+          kind, reason ->
+            stack = __STACKTRACE__
+
+            _ =
+              span
+              |> @span.set_sample_data("params", params)
+              |> @span.set_sample_data("environment", Appsignal.Metadata.metadata(socket))
+              |> @span.add_error(kind, reason, stack)
+              |> @tracer.close_span()
+
+            @tracer.ignore()
+            :erlang.raise(kind, reason, stack)
+        else
+          result ->
+            _ =
+              span
+              |> @span.set_sample_data("params", params)
+              |> @span.set_sample_data("environment", Appsignal.Metadata.metadata(socket))
+
+            result
+        end
+      end
+    )
+  end
 
   def live_view_action(module, name, socket, function) do
     instrument(module, name, socket, function)
