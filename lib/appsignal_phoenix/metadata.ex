d813ef33aae4b708194511bf1959e399c9f7a561
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2021-05-25 09:11:01 +0200

    Use Map.get in Appsignal.Metadata
    Matching on Phoenix.Socket and Phoenix.LiveView.Socket still matches
    when one of the keys doesn't exist. This prevents compilation errors
    when the sockets change again in the future.

--- lib/appsignal_phoenix/metadata.ex
+++ lib/appsignal_phoenix/metadata.ex
@@ -1,40 +1,26 @@
 defimpl Appsignal.Metadata, for: Phoenix.Socket do
-  def metadata(%Phoenix.Socket{
-        id: id,
-        channel: channel,
-        endpoint: endpoint,
-        handler: handler,
-        ref: ref,
-        topic: topic,
-        transport: transport
-      }) do
+  def metadata(%Phoenix.Socket{} = socket) do
     %{
-      "channel" => channel,
-      "endpoint" => endpoint,
-      "handler" => handler,
-      "id" => id,
-      "ref" => ref,
-      "topic" => topic,
-      "transport" => transport
+      "channel" => socket[:channel],
+      "endpoint" => socket[:endpoint],
+      "handler" => socket[:handler],
+      "id" => socket[:id],
+      "ref" => socket[:ref],
+      "topic" => socket[:topic],
+      "transport" => socket[:transport]
     }
   end
 end
 
 if Code.ensure_loaded?(Phoenix.LiveView) do
   defimpl Appsignal.Metadata, for: Phoenix.LiveView.Socket do
-    def metadata(%Phoenix.LiveView.Socket{
-          id: id,
-          root_view: root_view,
-          view: view,
-          endpoint: endpoint,
-          router: router
-        }) do
+    def metadata(%Phoenix.LiveView.Socket{} = socket) do
       %{
-        "id" => id,
-        "root_view" => root_view,
-        "view" => view,
-        "endpoint" => endpoint,
-        "router" => router
+        "id" => socket[:id],
+        "root_view" => socket[:root_view],
+        "view" => socket[:view],
+        "endpoint" => socket[:endpoint],
+        "router" => socket[:router]
       }
     end
   end
