95b0cb918ed8f1879f818a7aacfba4bbcb39a288
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2021-07-13 16:41:44 +0200

    Publish packages
    - v2.0.8
    
    [ci skip]

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Appsignal.Phoenix.MixProject do
   def project do
     [
       app: :appsignal_phoenix,
-      version: "2.0.7",
+      version: "2.0.8",
       description:
         "AppSignal's Phoenix instrumentation instruments calls to Phoenix applications to gain performance insights and error reporting",
       package: %{
