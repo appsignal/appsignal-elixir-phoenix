21bbc2333fc87358ee88528555458a14be05f249
AJ Foster <2789166+aj-foster@users.noreply.github.com>
2022-02-27 23:07:51 -0500

    Add auto-instrumentation of live views
    This enables one-line integration with AppSignal for live view
    and live component modules.

--- lib/appsignal_phoenix/live_view.ex
+++ lib/appsignal_phoenix/live_view.ex
@@ -1,7 +1,72 @@
 defmodule Appsignal.Phoenix.LiveView do
+  @moduledoc """
+  Instruments Phoenix live views and live components.
+
+  ## Usage
+
+  To automatically instrument a module with `Phoenix.LiveView` or `Phoenix.LiveComponent` callbacks,
+  add `use Appsignal.Phoenix.LiveView` to the module:
+
+      defmodule AppsignalPhoenixExampleWeb.ExampleLive do
+        use Appsignal.Phoenix.LiveView
+
+        def mount(params, session, socket) do
+          # ...
+        end
+      end
+
+  This will instrument many of the common callbacks used by these modules:
+
+    * `Phoenix.LiveView.mount/3`
+    * `Phoenix.LiveView.handle_event/3`
+    * `Phoenix.LiveView.handle_params/3`
+    * `Phoenix.LiveComponent.mount/1`
+    * `Phoenix.LiveComponent.update/2`
+
+  Alternatively, you can manually instrument functions in a live view or component using
+  `instrument/4`:
+
+      defmodule AppsignalPhoenixExampleWeb.ClockLive do
+        use Phoenix.LiveView
+        import Appsignal.Phoenix.LiveView, only: [instrument: 4]
+
+        def render(assigns) do
+          AppsignalPhoenixExampleWeb.ClockView.render("index.html", assigns)
+        end
+
+        def mount(_params, _session, socket) do
+          # Wrap the contents of the mount/2 function with a call to
+          # Appsignal.Phoenix.LiveView.instrument/4
+
+          instrument(__MODULE__, "mount", socket, fn ->
+            :timer.send_interval(1000, self(), :tick)
+            {:ok, assign(socket, state: Time.utc_now())}
+          end)
+        end
+
+        def handle_info(:tick, socket) do
+          # Wrap the contents of the handle_info/2 function with a call to
+          # Appsignal.Phoenix.LiveView.instrument/4:
+
+          instrument(__MODULE__, "tick", socket, fn ->
+            {:ok, assign(socket, state: Time.utc_now())}
+          end)
+        end
+      end
+
+  """
   @tracer Application.get_env(:appsignal, :appsignal_tracer, Appsignal.Tracer)
   @span Application.get_env(:appsignal, :appsignal_span, Appsignal.Span)
 
+  @doc false
+  defmacro __using__(_) do
+    quote do
+      use Appsignal.Phoenix.LiveView.AutoInstrument
+      @decorate_all instrument()
+    end
+  end
+
+  @doc false
   def instrument(module, name, socket, fun) do
     instrument(module, name, %{}, socket, fun)
   end
