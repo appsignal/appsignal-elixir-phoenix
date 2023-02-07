56a6b4670f4ca428d140040297e34376cb58c128
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2023-02-07 15:40:24 +0100

    Lock Appsignal Elixir to < 2.5.1
    AppSignal for Elixir adds new functions to Appsignal.Metadata, which
    aren't implemented on Appsignal for Phoenix on main, but are added in
    https://github.com/appsignal/appsignal-elixir-phoenix/pull/56.
    
    This patch locks to a version before 2.5.1 to fix the build until that
    pull request is merged.

--- mix.exs
+++ mix.exs
@@ -68,7 +68,7 @@ defmodule Appsignal.Phoenix.MixProject do
       end
 
     [
-      {:appsignal, ">= 2.4.0 and < 3.0.0"},
+      {:appsignal, ">= 2.4.0 and < 2.5.1"},
       {:appsignal_plug, ">= 2.0.12 and < 3.0.0"},
       {:phoenix, "~> 1.4"},
       {:phoenix_html, "~> 2.11 or ~> 3.0", optional: true},
