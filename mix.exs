b4e59c051c762fc57da4f3f508b70a68298e13b8
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2023-03-01 15:17:18 +0100

    Add Phoenix versions to CI matrix (#66)
    * Add Phoenix versions to CI matrix
    
    Add regresion testing to the CI to make sure features maintained for
    older versions of Phoenix keep working.
    
    This patch adds testing for Phoenix 1.7 and 1.6. The latter is run on
    the older Elixir versions, as Phoenix 1.7 requires Elixir 1.11 or
    higher.
    
    * Update mix.exs
    
    Co-authored-by: Tom de Bruijn <tom@tomdebruijn.com>
    
    ---------
    
    Co-authored-by: Tom de Bruijn <tom@tomdebruijn.com>

--- mix.exs
+++ mix.exs
@@ -66,7 +66,7 @@ defmodule Appsignal.Phoenix.MixProject do
     [
       {:appsignal, ">= 2.5.1 and < 3.0.0"},
       {:appsignal_plug, ">= 2.0.13 and < 3.0.0"},
-      {:phoenix, "~> 1.4"},
+      {:phoenix, System.get_env("PHOENIX_VERSION", "~> 1.4")},
       {:phoenix_html, "~> 2.11 or ~> 3.0", optional: true},
       {:phoenix_live_view, phoenix_live_view_version, optional: true},
       {:ex_doc, "~> 0.21", only: :dev, runtime: false},
